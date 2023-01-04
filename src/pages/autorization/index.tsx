fa393dd7553fb563a9edf6d360fba7a45243b38a
Iamaletdinov Artur <willbefine1@gmail.com>
2023-01-05 00:08:03 +0500

    добавлены временные фейковые функции регистрации и авторизации

--- src/pages/autorization/index.tsx
+++ src/pages/autorization/index.tsx
@@ -1,13 +1,27 @@
 import { useState } from "react";
+import { useNavigate } from "react-router";
 import "./autorization.scss";
 
 export function Autorization(): JSX.Element {
+  const navigate = useNavigate();
   const [formBxActive, setFormBxActive] = useState<string>("");
   const [startPageActive, setStartPageActive] = useState<string>("");
 
   function toGoSignupPage(isActive: boolean): void {
     setFormBxActive(isActive ? "autorization__formBx--active" : "");
     setStartPageActive(isActive ? "autorization--active" : "");
+  }
+
+  async function toSignin(e: React.SyntheticEvent<EventTarget>): Promise<void> {
+    e.preventDefault();
+    await alert("Вы авторизированы");
+    navigate("/");
+  }
+
+  async function toSignup(e: React.SyntheticEvent<EventTarget>): Promise<void> {
+    e.preventDefault();
+    await alert("Вы зарегестрированы");
+    toGoSignupPage(false);
   }
 
   return (
@@ -29,22 +43,26 @@ export function Autorization(): JSX.Element {
         </div>
         <div className={`autorization__formBx ${formBxActive}`}>
           <div className="autorization__form autorization__signinForm">
-            <form>
+            <form onSubmit={toSignin}>
               <input type="text" placeholder="Login" />
               <input type="password" placeholder="Password" />
-              <input type="submit" value="Войти" />
+              <input type="submit" value="Войти" onClick={toSignin} />
               <button className="autorization__forget-password">
                 Забыли пароль?
               </button>
             </form>
           </div>
           <div className="autorization__form autorization__signupForm">
-            <form>
+            <form onSubmit={toSignup}>
               <input type="text" placeholder="Username" />
               <input type="text" placeholder="Email address" />
               <input type="password" placeholder="Password" />
               <input type="password" placeholder="Confirm password" />
-              <input type="submit" value="Зарегестрироваться" />
+              <input
+                type="submit"
+                value="Зарегестрироваться"
+                onClick={toSignup}
+              />
             </form>
           </div>
         </div>
