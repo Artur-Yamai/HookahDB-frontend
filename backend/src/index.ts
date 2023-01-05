fa336eeb66ede4ce4dcdf4204b3cef03a65c6646
Iamaletdinov Artur <willbefine1@gmail.com>
2023-01-05 23:03:05 +0500

    added cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,10 +2,16 @@ import express, { Express } from "express";
 import { registerValidation } from "./validations";
 import { UserController } from "./controllers";
 import { handleValidationErrors } from "./utils";
+import cors from "cors";
 
-const port: number = 6000;
+const port: number = 6060;
 const app: Express = express();
 app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("<h1>In process</h1>");
+});
 
 app.post(
   "/user/register",
