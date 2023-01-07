76d2c30e993e41c1705733ee51039285395a65c9
Iamaletdinov Artur <willbefine1@gmail.com>
2023-01-07 23:05:48 +0500

    Добавлена схема юзера

---
+++ backend/src/models/User.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    login: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    fullName: String,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    passwordHash: {
+      type: String,
+      required: true,
+    },
+    avatarUrl: String,
+    roleCode: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("User", UserSchema);
