a7562c677b48f7b70eb01bfbc863570e5037ae75
Iamaletdinov Artur <willbefine1@gmail.com>
2023-01-07 23:06:24 +0500

    замена поля username =>login

--- backend/src/validations/index.ts
+++ backend/src/validations/index.ts
@@ -6,7 +6,7 @@ export const registerValidation = [
   body("password", "Пароль должен быть минимум 5 символов").isLength({
     min: 5,
   }),
-  body("username", "Ник должен содержать от 4 до 30 символов").isLength({
+  body("login", "Ник должен содержать от 4 до 30 символов").isLength({
     min: 4,
     max: 30,
   }),
