d1a74b80754f97e2e99640ccd3527dd8a8155373
Iamaletdinov Artur <willbefine1@gmail.com>
2023-01-07 23:07:16 +0500

    реализован метод регистрации юзера на backend

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,8 +1,47 @@
-// import jwt from "jsonwebtoken";
 import { Response, Request } from "express";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import UserModel from "../models/User";
+import { jwtSectretKey } from "../sectets";
 
-export const register = (req: Request, res: Response) => {
-  console.log("Регистрация прошла успешно");
+interface IUserRegister {
+  email: string;
+  password: string;
+  login: string;
+}
 
-  res.json({ success: true });
+export const register = async (req: Request, res: Response) => {
+  try {
+    const { email, password, login }: IUserRegister = req.body;
+
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash: string = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+      email,
+      passwordHash,
+      login,
+    });
+
+    const user = await doc.save();
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      jwtSectretKey,
+      {
+        expiresIn: "30d",
+      }
+    );
+
+    res.json({ success: true, token });
+  } catch (error) {
+    console.log("error registration", error);
+    res.status(500).json({
+      message: "Не удалось зарегестриролваться",
+      success: false,
+      error,
+    });
+  }
 };
