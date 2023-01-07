c25701961382727fe930a9c1a96960dda033cddb
Iamaletdinov Artur <willbefine1@gmail.com>
2023-01-07 14:24:15 +0500

    Изменено тело ответа при ошибке и при корректном ответе

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -4,5 +4,5 @@ import { Response, Request } from "express";
 export const register = (req: Request, res: Response) => {
   console.log("Регистрация прошла успешно");
 
-  res.json({ status: true });
+  res.json({ success: true });
 };
