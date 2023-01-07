9834d314212c803b7227efe076693fbf35be7d11
Iamaletdinov Artur <willbefine1@gmail.com>
2023-01-07 19:53:33 +0500

    ПРиходит ответ неправильно заполненных полей при регистрации

--- backend/src/utils/handleValidationErrors.ts
+++ backend/src/utils/handleValidationErrors.ts
@@ -4,8 +4,11 @@ import { Response, Request, NextFunction } from "express";
 export default (req: Request, res: Response, next: NextFunction) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array());
-  } else {
-    next();
+    return res.status(400).json({
+      success: false,
+      data: errors.array(),
+    });
   }
+
+  next();
 };
