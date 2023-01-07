27071e7045047ac4642303ab183fd75803b1c778
Iamaletdinov Artur <willbefine1@gmail.com>
2023-01-07 23:10:25 +0500

    в header запроса добавлен token, если он сохранен в localstorage

--- src/store/axios.ts
+++ src/store/axios.ts
@@ -1,6 +1,26 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 const baseURL = "http://localhost:6060";
 
+type IConfig = AxiosRequestConfig & {
+  headers?: {
+    Authorization?: string;
+  };
+};
+
 const instanse = axios.create({ baseURL });
+
+instanse.interceptors.request.use((config: AxiosRequestConfig): IConfig => {
+  const token: string | undefined | null = localStorage.getItem("token");
+  if (typeof token === "string") {
+    const newConfig: IConfig = { ...config };
+    if (!newConfig.headers) {
+      newConfig.headers = {};
+    }
+    newConfig.headers.Authorization = token;
+
+    return newConfig;
+  }
+  return config;
+});
 
 export default instanse;
