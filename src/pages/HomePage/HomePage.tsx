777c23f049894c972b3a6eb60ecf66e28d89c4a9
Iamaletdinov Artur <willbefine1@gmail.com>
2023-01-26 21:17:16 +0500

    GUI: добавлены блоки, для симуляции отображения данных на главной

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,3 +1,17 @@
+import "./HomePage.scss";
+
 export function HomePage() {
-  return <div>HomePage</div>;
+  const arr: number[] = [];
+
+  for (let i: number = 0; i < 16; i++) {
+    arr.push(i);
+  }
+
+  return (
+    <div className="homepage">
+      {arr.map((_, i) => (
+        <div key={i} className="homepage__item"></div>
+      ))}
+    </div>
+  );
 }
