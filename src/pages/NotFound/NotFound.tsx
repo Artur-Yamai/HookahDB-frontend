c73f0e9bf10fcd6e91ccbeb85db3cf022fd21d77
Iamaletdinov Artur <willbefine1@gmail.com>
2023-01-28 20:56:46 +0500

    GUI: немного доработаны стили для страницы NotFound

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -4,7 +4,7 @@ export function NotFound() {
   return (
     <div className="not-found">
       <h1>404</h1>
-      <p>oops! Страница не найдена </p>
+      <p>Упс! Страница не найдена </p>
     </div>
   );
 }
