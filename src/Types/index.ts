46b08812c0e67a7418019ab101c210cd1533f742
Iamaletdinov Artur <willbefine1@gmail.com>
2023-01-28 21:49:13 +0500

    GUI: Добавлен interface ITobacco

--- src/Types/index.ts
+++ src/Types/index.ts
@@ -3,5 +3,6 @@ import {
   IRegistrationUserData,
 } from "./user/AuthAndRegInterfaces";
 import { IUser } from "./user/User";
+import { ITobacco } from "./tobacco/tobacco";
 
-export type { IAuthorizationUserData, IRegistrationUserData, IUser };
+export type { IAuthorizationUserData, IRegistrationUserData, IUser, ITobacco };
---
+++ src/Types/tobacco/tobacco.ts
@@ -0,0 +1,10 @@
+export interface ITobacco {
+  _id: string;
+  name: string;
+  fabricator: string;
+  description: string;
+  photosUrl: string[];
+  userId: string;
+  createdAt: string;
+  updatedAt: string;
+}
