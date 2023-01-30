b703aead923fb5891e8b856d32932b943db08cb2
Iamaletdinov Artur <willbefine1@gmail.com>
2023-01-30 13:19:38 +0500

    B: добавлен логгер

---
+++ backend/src/logger/logger.service.ts
@@ -0,0 +1,70 @@
+import { Router } from "express";
+import chalk from "chalk";
+
+type HTTPMehtod = keyof Pick<
+  Router,
+  "get" | "post" | "delete" | "patch" | "put"
+>;
+
+const lessThanTen = (n: number): string => (n < 10 ? `0${n}` : `${n}`);
+
+class LoggerService {
+  private getDate(): string {
+    const now: Date = new Date();
+    const year: number = now.getFullYear();
+    const month: number = now.getMonth() + 1;
+    const day: number = now.getDate();
+    const H: number = now.getHours();
+    const M: number = now.getMinutes();
+    const S: number = now.getSeconds();
+    const ms: number = now.getMilliseconds();
+
+    return `${lessThanTen(day)}-${lessThanTen(month)}-${year} ${lessThanTen(
+      H
+    )}:${lessThanTen(M)}:${lessThanTen(S)}.${ms}`;
+  }
+
+  private logger(...args: unknown[]): void {
+    console.log(this.getDate(), ...args);
+  }
+
+  public log(...args: unknown[]): void {
+    this.logger(chalk.bgBlue(" INFO "), ...args);
+  }
+
+  public error(...args: unknown[]): void {
+    this.logger(chalk.bgRed(" ERROR "), ...args);
+  }
+
+  public warn(...args: unknown[]): void {
+    this.logger(chalk.bgYellow(" WARN "), ...args);
+  }
+
+  public success(
+    HTTPMehtod: HTTPMehtod,
+    HTTPPath: string,
+    body: unknown = ""
+  ): void {
+    let method = HTTPMehtod.toUpperCase();
+    switch (HTTPMehtod) {
+      case "get":
+        method = chalk.blue(method);
+        break;
+      case "put":
+        method = chalk.rgb(255, 165, 0)(method);
+        break;
+      case "patch":
+        method = chalk.yellow(method);
+        break;
+      case "delete":
+        method = chalk.red(method);
+        break;
+      case "post":
+        method = chalk.green(method);
+        break;
+    }
+    this.log(method, HTTPPath, `\t${body}`);
+  }
+}
+
+export default new LoggerService();
