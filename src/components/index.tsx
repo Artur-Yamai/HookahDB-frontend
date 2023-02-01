6924184b4324d665e11ca18616672e9f6119dce9
Iamaletdinov Artur <willbefine1@gmail.com>
2023-02-01 16:35:22 +0500

    GUI: добавлен TobaccoInfo в index

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,4 +2,4 @@ export * as NavBar from "./NavBar/NavBar";
 export { MainLayout } from "./MainLayout/MainLayout";
 export { AuthorizationForm } from "./AuthorizationForm/AuthorizationForm";
 export { RegistrationForm } from "./RegistrationForm/RegistrationForm";
-export { TobaccosList } from "./TobaccoCmponents";
+export { TobaccosList, TobaccoInfo } from "./TobaccoCmponents";
