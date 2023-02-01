95ccccd72d16aa67919f5cf9daa1692c64ed6c25
Iamaletdinov Artur <willbefine1@gmail.com>
2023-02-01 12:08:55 +0500

    GUI: добавлены CRUD для тобаков

--- src/API/TobaccoApi.ts
+++ src/API/TobaccoApi.ts
@@ -2,4 +2,18 @@ import axios from "./axios";
 
 export const TobaccoApi = {
   getAllTobaccos: async () => await axios.get("/tobaccos"),
+
+  getTobaccoEndpoint: (id: string) => `/tobacco/${id}`,
+  async getTobacco(id: string) {
+    return await axios.get(this.getTobaccoEndpoint(id));
+  },
+  async updateTobacco(id: string) {
+    return await axios.delete(this.getTobaccoEndpoint(id));
+  },
+  async createTobacco(id: string) {
+    return await axios.delete(this.getTobaccoEndpoint(id));
+  },
+  async delteTobacco(id: string) {
+    return await axios.delete(this.getTobaccoEndpoint(id));
+  },
 };
