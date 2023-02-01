c163d7c4d2ef30069d5b03a717c5ae5bebb06398
Iamaletdinov Artur <willbefine1@gmail.com>
2023-02-01 16:14:53 +0500

    GUI: доработаны CRUD табака (всяко не в последний раз)

--- src/API/TobaccoApi.ts
+++ src/API/TobaccoApi.ts
@@ -1,18 +1,53 @@
+import { INewTobacco, ITobacco } from "../Types";
 import axios from "./axios";
 
 export const TobaccoApi = {
   getAllTobaccos: async () => await axios.get("/tobaccos"),
 
+  createTobacco: async (
+    userId: string,
+    tobaccoData: INewTobacco,
+    photos: File[]
+  ) => {
+    const formData = new FormData();
+
+    formData.append("userId", userId);
+
+    const object: any = { ...tobaccoData };
+    for (const key in object) {
+      formData.append(key, object[key]);
+    }
+
+    photos.map((photo: File) => formData.append("photos", photo));
+
+    return await axios.post("/tobacco");
+  },
+
   getTobaccoEndpoint: (id: string) => `/tobacco/${id}`,
+
+  async updateTobacco(
+    updaterUserId: string,
+    tobaccoData: ITobacco,
+    photos: File[]
+  ) {
+    const formData = new FormData();
+
+    formData.append("updaterUserId", updaterUserId);
+
+    const object: any = { ...tobaccoData };
+    for (const key in object) {
+      formData.append(key, object[key]);
+    }
+
+    photos.map((photo: File) => formData.append("photos", photo));
+
+    return await axios.put(this.getTobaccoEndpoint(object._id));
+  },
+
   async getTobacco(id: string) {
     return await axios.get(this.getTobaccoEndpoint(id));
   },
-  async updateTobacco(id: string) {
-    return await axios.delete(this.getTobaccoEndpoint(id));
-  },
-  async createTobacco(id: string) {
-    return await axios.delete(this.getTobaccoEndpoint(id));
-  },
+
   async delteTobacco(id: string) {
     return await axios.delete(this.getTobaccoEndpoint(id));
   },
