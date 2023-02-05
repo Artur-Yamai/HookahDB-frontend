ff71bc4a30919f21c478337517d415b2e049bdb6
Iamaletdinov Artur <willbefine1@gmail.com>
2023-02-05 12:26:39 +0500

    GUI: добавлен prop alt для img

--- src/UI/Picture/Picture.tsx
+++ src/UI/Picture/Picture.tsx
@@ -2,14 +2,15 @@ import config from "../../configuration";
 
 interface IImage {
   url?: string;
+  alt?: string;
 }
 
-export function Picture({ url }: IImage): JSX.Element {
+export function Picture({ url, alt = "Изображение" }: IImage): JSX.Element {
   const avatarUrl = url ? config.photoUrl + url : "noimg.jpg";
 
   return (
     <picture className="w100">
-      <img src={avatarUrl} alt="аватар" className="w100" />
+      <img src={avatarUrl} alt={alt} className="w100" />
     </picture>
   );
 }
