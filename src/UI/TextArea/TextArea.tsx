b1988a25fe1b78907fee9e9eb60204e2945c5db8
Iamaletdinov Artur <willbefine1@gmail.com>
2023-02-09 19:25:21 +0500

    GUI: add TextArea component

---
+++ src/UI/TextArea/TextArea.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import "./TextArea.scss";
+
+interface ITextArea {
+  text: string;
+  label: string;
+  placeholder?: string;
+  required?: boolean;
+  onChange: (value: string) => void;
+}
+
+export function TextArea({
+  text,
+  label,
+  onChange,
+  placeholder = "",
+  required = true,
+}: ITextArea): JSX.Element {
+  const [value, changeValue] = useState<string>(text);
+  const id = label + "-" + Math.round(Math.random() * 100000);
+
+  function change(e: React.ChangeEvent<HTMLTextAreaElement>) {
+    const newValue: string = e.target.value;
+    changeValue(newValue);
+    onChange(newValue);
+  }
+
+  return (
+    <div className="textarea">
+      <textarea
+        required={required}
+        placeholder={placeholder}
+        value={value}
+        onChange={change}
+        name="textarea"
+        id=""
+        cols={30}
+        rows={10}
+      />
+      <label className="textarea__label">{label}</label>
+    </div>
+  );
+}
--- src/UI/index.ts
+++ src/UI/index.ts
@@ -5,3 +5,4 @@ export { InputTypeFIle } from "./InputTypeFIle/InputTypeFIle";
 export { StartingSpinner } from "./StartingSpinner/StartingSpinner";
 export { notify } from "./notify/notify";
 export { confirm, Popup } from "./Dialogs";
+export { TextArea } from "./TextArea/TextArea";
