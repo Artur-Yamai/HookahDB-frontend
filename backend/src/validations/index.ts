a066d7c6bad7fd304cc6f7c5474c60658c80db11
Iamaletdinov Artur <willbefine1@gmail.com>
2023-02-10 15:31:27 +0500

    B: registerValidation "Ник" => "Логин"

--- backend/src/validations/index.ts
+++ backend/src/validations/index.ts
@@ -5,7 +5,7 @@ export const registerValidation = [
   body("password", "Пароль должен быть минимум 5 символов").isLength({
     min: 5,
   }),
-  body("login", "Ник должен содержать от 4 до 30 символов").isLength({
+  body("login", "Логин должен содержать от 4 до 30 символов").isLength({
     min: 4,
     max: 30,
   }),
