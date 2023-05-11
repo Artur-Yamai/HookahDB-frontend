1045bf6e629a212e97e045086247fe1944454f2c
Iamaletdinov Artur <willbefine1@gmail.com>
2023-05-11 12:06:03 +0500

    GUI: для компонента picture добавлен пропс className

--- src/UI/Picture/Picture.tsx
+++ src/UI/Picture/Picture.tsx
@@ -3,13 +3,18 @@ import config from "../../configuration";
 interface IImage {
   url?: string;
   alt?: string;
+  className?: string;
 }
 
-export function Picture({ url, alt = "Изображение" }: IImage): JSX.Element {
+export function Picture({
+  url,
+  className,
+  alt = "Изображение",
+}: IImage): JSX.Element {
   const avatarUrl = url ? config.photoUrl + url : "noimg.jpg";
 
   return (
-    <picture className="w100">
+    <picture className={`${className} w100`}>
       <img src={avatarUrl} alt={alt} className="w100" />
     </picture>
   );
