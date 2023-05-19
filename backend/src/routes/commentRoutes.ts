4f70fc810414e7093d035968a0f2d105f7e22330
Iamaletdinov Artur <willbefine1@gmail.com>
2023-05-19 13:37:32 +0500

    B: кдвлены неиспользуемые роуты для комментариев

--- backend/src/routes/commentRoutes.ts
+++ backend/src/routes/commentRoutes.ts
@@ -6,10 +6,8 @@ import { checkAuth } from "../utils";
 const router = Router();
 const upload: multer.Multer = multer();
 
-router.get("/api/comment/:id", CommentController.getById);
 router
   .route("/api/comments")
-  .get(CommentController.getAll)
   .post(upload.none(), checkAuth, CommentController.create)
   .put(upload.none(), checkAuth, CommentController.update)
   .delete(checkAuth, CommentController.remove);
