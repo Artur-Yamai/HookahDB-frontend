7c7532137d9b6a29f226fd25a7a672ae1eebd73f
Iamaletdinov Artur <willbefine1@gmail.com>
2023-06-09 15:36:34 +0500

    B: folder rensme Userpage->UserPage

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,5 @@
 import Authorization from "./Authorization/Authorization";
-import Userpage from "./Userpage/Userpage";
+import Userpage from "./UserPage/Userpage";
 import TobaccoPage from "./TobaccoPage/TobaccoPage";
 import ForHookah from "./ForHookah/ForHookah";
 export { HomePage } from "./HomePage/HomePage";
