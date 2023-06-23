eca49a2f5911ed5304e32c32023a0d4dfc1a3799
Iamaletdinov Artur <willbefine1@gmail.com>
2023-06-23 17:51:04 +0500

    deleted trash

--- backend/src/controllers/index.ts
+++
@@ -1,5 +0,0 @@
-export * as UserController from "./UserController";
-export * as TobaccoController from "./TobaccoController";
-export * as CommentController from "./CommentController";
-export * as FavoritesTobaccoController from "./FavoritesTobaccoController";
-export * as RatingController from "./RatingController";
