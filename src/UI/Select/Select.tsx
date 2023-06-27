e90886f87bef62f78739e762d0677dc23e883a86
Iamaletdinov Artur <willbefine1@gmail.com>
2023-06-27 20:35:25 +0500

    в Select передается только значение, а не выбранный объект

--- src/UI/Select/Select.tsx
+++ src/UI/Select/Select.tsx
@@ -1,9 +1,11 @@
+import { useState, useEffect } from "react";
 import ReactSelect, { SingleValue } from "react-select";
 import { SelectProps } from "./SelectProps";
 import "./Select.scss";
 
 export const Select = ({
   options,
+  value,
   onChange,
   isLoading = false,
   isClearable = false,
@@ -11,12 +13,24 @@ export const Select = ({
   valueKey = "value",
   labelKey = "label",
 }: SelectProps) => {
+  const [intermediateValue, setIntermediateValue] =
+    useState<SingleValue<any>>();
+
   const toChange = (newValue: SingleValue<any>) => {
     onChange(newValue);
+    setIntermediateValue(newValue);
   };
+
+  useEffect(() => {
+    const res = options.find((option) => option[valueKey] === value);
+    if (res) {
+      setIntermediateValue(res);
+    }
+  }, [options, valueKey, value]);
 
   return (
     <ReactSelect
+      value={intermediateValue}
       classNamePrefix="custom-select"
       isLoading={isLoading}
       options={options}
--- src/UI/Select/SelectProps.ts
+++ src/UI/Select/SelectProps.ts
@@ -2,10 +2,11 @@ import { SingleValue } from "react-select";
 
 export interface SelectProps {
   options: any[];
+  value: any;
   onChange: (newValue: SingleValue<any>) => void;
   placeholder?: string;
   isLoading?: boolean;
   isClearable?: boolean;
-  valueKey: string;
-  labelKey: string;
+  valueKey?: string;
+  labelKey?: string;
 }
