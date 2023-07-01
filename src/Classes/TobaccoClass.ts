67e982d71290cea1af66001ab254f527de10e76a
Iamaletdinov Artur <willbefine1@gmail.com>
2023-07-01 21:16:07 +0500

    TobaccoClass - чуть меньшая многословность

--- src/Classes/TobaccoClass.ts
+++ src/Classes/TobaccoClass.ts
@@ -4,17 +4,19 @@ export class TobaccoClass
   implements
     Pick<ITobacco, "name" | "fabricatorId" | "description" | "photoUrl" | "id">
 {
-  name: string;
-  fabricatorId: string;
-  description: string;
-  photoUrl: string;
-  id: string;
+  name: string = "";
+  fabricatorId: string = "";
+  description: string = "";
+  photoUrl: string = "";
+  id: string = "";
 
-  constructor(tobacco: TobaccoClass | null) {
-    this.name = tobacco?.name ?? "";
-    this.fabricatorId = tobacco?.fabricatorId ?? "";
-    this.description = tobacco?.description ?? "";
-    this.photoUrl = tobacco?.photoUrl ?? "";
-    this.id = tobacco?.id ?? "";
+  constructor(tobacco: ITobacco | null) {
+    if (!tobacco) return;
+
+    this.name = tobacco.name;
+    this.fabricatorId = tobacco.fabricatorId;
+    this.description = tobacco.description;
+    this.photoUrl = tobacco.photoUrl;
+    this.id = tobacco.id;
   }
 }
