aed5757e01f3593bb8af8313348ccd3ae201a4b3
Iamaletdinov Artur <willbefine1@gmail.com>
2023-07-13 17:29:37 +0500

    Picture: обработка ошибок при неполучении изображения с сервера

--- src/UI/Picture/Picture.tsx
+++ src/UI/Picture/Picture.tsx
@@ -1,20 +1,34 @@
+import { useState } from "react";
 import config from "../../configuration";
+import { useMount } from "../../hooks";
 
 interface PictureProps {
   url?: string;
   alt?: string;
   className?: string;
+  onClick?: () => void;
 }
 
 export const Picture = ({
   url,
   className,
   alt = "Изображение",
+  onClick,
 }: PictureProps): JSX.Element => {
-  const avatarUrl = url ? config.photoUrl + url : "noimg.jpg";
+  const [avatarUrl, setAvatarUrl] = useState<string>("noimg.jpg");
+
+  useMount(() => {
+    const img = new Image();
+    const path = config.photoUrl + url;
+
+    img.onload = () => setAvatarUrl(path);
+    img.onerror = () => setAvatarUrl("noimg.jpg");
+
+    img.src = path;
+  });
 
   return (
-    <picture className={`${className} w100`}>
+    <picture className={`${className} w100`} onClick={onClick}>
       <img src={avatarUrl} alt={alt} className="w100" />
     </picture>
   );
