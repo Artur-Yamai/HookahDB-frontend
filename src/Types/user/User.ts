15603a622632baafe92968fdfe552e74bfdb394c
Iamaletdinov Artur <willbefine1@gmail.com>
2023-08-17 11:03:14 +0500

    add refCode in User interface

--- src/Types/user/User.ts
+++ src/Types/user/User.ts
@@ -1,11 +1,12 @@
 import { GUID } from "../GUID";
 
 export interface User {
+  id: GUID;
   createdAt: string;
   email: string;
   login: string;
   roleCode: number;
+  refCode: string | null;
   avatarUrl: string;
   updatedAt: string;
-  id: GUID;
 }
