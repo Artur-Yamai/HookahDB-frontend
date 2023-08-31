7ce920552b7823ad8e3a7fd01bd4aa8213bf3d01
Iamaletdinov Artur <willbefine1@gmail.com>
2023-08-31 17:33:28 +0500

    удалена папка Classes, т.к. нигде не используется

--- src/Classes/CoalClass.ts
+++
@@ -1,9 +0,0 @@
-import { Coal } from "../Types";
-import { ProductClass } from "./ProductClass";
-
-export class CoalClass extends ProductClass {
-  // eslint-disable-next-line
-  constructor(coal?: Coal | null) {
-    super(coal);
-  }
-}
--- src/Classes/TobaccoClass.ts
+++
@@ -1,9 +0,0 @@
-import { Tobacco } from "../Types";
-import { ProductClass } from "./ProductClass";
-
-export class TobaccoClass extends ProductClass {
-  // eslint-disable-next-line
-  constructor(tobacco?: Tobacco | null) {
-    super(tobacco);
-  }
-}
