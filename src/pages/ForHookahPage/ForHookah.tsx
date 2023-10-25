4938da197b85167eb5a0e180bd5cbebd12e3f801
Iamaletdinov Artur <willbefine1@gmail.com>
2023-10-25 15:24:25 +0500

    Диалог выбора создаваемой сущности возвращает тип создаваемой сущности

--- src/pages/ForHookahPage/ForHookah.tsx
+++ src/pages/ForHookahPage/ForHookah.tsx
@@ -2,13 +2,18 @@ import { useState } from "react";
 import { observer } from "mobx-react-lite";
 import { useSearchParams } from "react-router-dom";
 import { Helmet } from "react-helmet";
-import { useMount, useUnmount } from "../../hooks";
-import TobaccoStore from "../../store/tobacco";
-import CoalStore from "../../store/coal";
-import { FilterPanel, TobaccosList, CoalList } from "../../components";
-import { ProductListName, SelectOption } from "../../Types";
-import { TobaccoEditDialog, CoalEditDialog } from "../../components";
-import { RoleCodes, rightsCheck } from "../../helpers";
+import { useMount, useUnmount } from "hooks";
+import TobaccoStore from "store/tobacco";
+import CoalStore from "store/coal";
+import {
+  FilterPanel,
+  TobaccosList,
+  CoalList,
+  AddSelectionDialog,
+} from "components";
+import { ProductListName, SelectOption, AddedEntitiesTypes } from "Types";
+import { TobaccoEditDialog, CoalEditDialog } from "components";
+import { RoleCodes, rightsCheck } from "helpers";
 import "./ForHookah.scss";
 
 export const ForHookah: React.FC = observer(() => {
@@ -23,6 +28,10 @@ export const ForHookah: React.FC = observer(() => {
 
   const [selectedOption, setSelectedOption] = useState<SelectOption>(option);
   const [isVisibleDialog, toggleVisibleDialog] = useState<boolean>(false);
+  const [isVisibleSelectionDialog, toggleVisibleSelectionDialog] =
+    useState<boolean>(false);
+  const [entityDialogType, setEntityDialogType] =
+    useState<AddedEntitiesTypes>();
 
   const onChange = (option: SelectOption): void => {
     setSelectedOption(option);
@@ -30,23 +39,18 @@ export const ForHookah: React.FC = observer(() => {
     getData(option.value);
   };
 
-  useMount(() => {
-    getData(selectedOption.value);
-  });
+  useMount(() => getData(selectedOption.value));
+  useUnmount(() => clearData());
 
-  useUnmount(() => {
-    clearData();
-  });
-
-  const getData = async (productName: ProductListName): Promise<void> => {
+  const getData = (productName: ProductListName): void => {
     clearData();
 
     switch (productName) {
       case "tobaccos":
-        await TobaccoStore.getAllTobaccos();
+        TobaccoStore.getAllTobaccos();
         break;
       case "coals":
-        await CoalStore.getAllCoals();
+        CoalStore.getAllCoals();
         break;
     }
   };
@@ -54,6 +58,12 @@ export const ForHookah: React.FC = observer(() => {
   const clearData = (): void => {
     TobaccoStore.clearTobaccoList();
     CoalStore.clearCoalList();
+  };
+
+  const openEntityDialog = (entity: AddedEntitiesTypes) => {
+    toggleVisibleSelectionDialog(false);
+    setEntityDialogType(entity);
+    toggleVisibleDialog(true);
   };
 
   return (
@@ -68,32 +78,39 @@ export const ForHookah: React.FC = observer(() => {
           value={selectedOption}
         />
         {(selectedOption.value === "tobaccos" && (
-          <>
-            <TobaccoEditDialog
-              tobacco={TobaccoStore.tobacco}
-              isVisible={isVisibleDialog}
-              closeDialog={() => toggleVisibleDialog(false)}
-            />
-            <TobaccosList tobaccos={TobaccoStore.tobaccos} />
-          </>
+          <TobaccosList tobaccos={TobaccoStore.tobaccos} />
         )) ||
           (selectedOption.value === "coals" && (
-            <>
-              <CoalEditDialog
-                coal={CoalStore.coal}
+            <CoalList coals={CoalStore.coals} />
+          ))}
+        {rightsCheck(RoleCodes.moderator) && (
+          <>
+            <button
+              className="for-hookah__add-button"
+              onClick={() => toggleVisibleSelectionDialog(true)}
+            >
+              +
+            </button>
+            <AddSelectionDialog
+              isVisible={isVisibleSelectionDialog}
+              closeDialog={() => toggleVisibleSelectionDialog(false)}
+              agree={(entity: AddedEntitiesTypes) => openEntityDialog(entity)}
+            />
+            {(entityDialogType === "tobacco" && (
+              <TobaccoEditDialog
+                tobacco={TobaccoStore.tobacco}
                 isVisible={isVisibleDialog}
                 closeDialog={() => toggleVisibleDialog(false)}
               />
-              <CoalList coals={CoalStore.coals} />
-            </>
-          ))}
-        {rightsCheck(RoleCodes.moderator) && (
-          <button
-            className="for-hookah__add-button"
-            onClick={() => toggleVisibleDialog(true)}
-          >
-            +
-          </button>
+            )) ||
+              (entityDialogType === "coal" && (
+                <CoalEditDialog
+                  coal={CoalStore.coal}
+                  isVisible={isVisibleDialog}
+                  closeDialog={() => toggleVisibleDialog(false)}
+                />
+              ))}
+          </>
         )}
       </div>
     </>
