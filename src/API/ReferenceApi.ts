83b6e3ec4c17c735fe7d8ce1a6dcfaad8886cc3d
Iamaletdinov Artur <willbefine1@gmail.com>
2023-10-25 17:42:55 +0500

    Добавлен метод добавления/редактирования производителя

--- src/API/ReferenceApi.ts
+++ src/API/ReferenceApi.ts
@@ -1,9 +1,10 @@
-import { Reference } from "../Types";
+import { Reference } from "Types";
 import RepositoryHelper from "../helpers/RepositoryHelper";
 const resource = "/reference";
 
 export const ReferenceApi = {
   getReferenceEndpoint: (name: string) => `${resource}/${name}`,
+
   async getReference(name: string): Promise<Reference[] | null> {
     try {
       const { data } = await RepositoryHelper.get(
@@ -12,6 +13,21 @@ export const ReferenceApi = {
       return data.success ? data.body : null;
     } catch (_) {
       return null;
+    }
+  },
+
+  async saveReferenceValue(
+    referenceName: string,
+    ref: Reference | Pick<Reference, "value">
+  ): Promise<boolean> {
+    try {
+      const { data } = await RepositoryHelper.save(
+        ref,
+        this.getReferenceEndpoint(referenceName)
+      );
+      return data.success;
+    } catch (_) {
+      return false;
     }
   },
 };
