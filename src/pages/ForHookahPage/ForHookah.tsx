9501577559780f5178a5ed05fb4f134a17030807
Iamaletdinov Artur <willbefine1@gmail.com>
2023-11-06 18:25:34 +0500

    Добавлена кнопка фильтра на страницу товаров

--- src/pages/ForHookahPage/ForHookah.tsx
+++ src/pages/ForHookahPage/ForHookah.tsx
@@ -2,12 +2,13 @@ import { useState } from "react";
 import { observer } from "mobx-react-lite";
 import { useSearchParams } from "react-router-dom";
 import { Helmet } from "react-helmet";
+import { RiFilter3Fill } from "react-icons/ri";
 import { useMount, useUnmount } from "hooks";
 import TobaccoStore from "store/tobacco";
 import CoalStore from "store/coal";
 import { TobaccosList, CoalList, EntitySelectionAndCreation } from "components";
 import { TabPanel } from "UI";
-import { ProductListName, SelectOption } from "Types";
+import { Product, ProductListName, SelectOption } from "Types";
 import { RoleCodes, rightsCheck } from "helpers";
 import "./ForHookah.scss";
 
@@ -17,6 +18,8 @@ export const ForHookah: React.FC = observer(() => {
     { value: "coals", label: "Угли" },
   ];
   const [searchParams, setSearchParams] = useSearchParams();
+
+  const [productList, setProductList] = useState<Product[]>([]);
 
   const listName = searchParams.get("list-name");
   const option = options.find((opt) => opt.value === listName) ?? options[0];
@@ -33,20 +36,23 @@ export const ForHookah: React.FC = observer(() => {
   useMount(() => getData(selectedOption.value));
   useUnmount(() => clearData());
 
-  const getData = (productName: ProductListName): void => {
+  const getData = async (productName: ProductListName): Promise<void> => {
     clearData();
 
     switch (productName) {
       case "tobaccos":
-        TobaccoStore.getAllTobaccos();
+        await TobaccoStore.getAllTobaccos();
+        setProductList(TobaccoStore.tobaccos);
         break;
       case "coals":
-        CoalStore.getAllCoals();
+        await CoalStore.getAllCoals();
+        setProductList(CoalStore.coals);
         break;
     }
   };
 
   const clearData = (): void => {
+    setProductList([]);
     TobaccoStore.clearTobaccoList();
     CoalStore.clearCoalList();
   };
@@ -61,27 +67,36 @@ export const ForHookah: React.FC = observer(() => {
           options={options}
           onClick={onChange}
           defaultOption={selectedOption}
-        />
-        {(selectedOption.value === "tobaccos" && (
-          <TobaccosList tobaccos={TobaccoStore.tobaccos} />
-        )) ||
-          (selectedOption.value === "coals" && (
-            <CoalList coals={CoalStore.coals} />
-          ))}
-        {rightsCheck(RoleCodes.moderator) && (
+        >
           <>
             <button
-              className="for-hookah__add-button"
-              onClick={() => toggleVisibleDialog(true)}
+              className="for-hookah__button  for-hookah__button--filter"
+              onClick={() => console.log("show filter")}
             >
-              +
+              <RiFilter3Fill />
             </button>
-            <EntitySelectionAndCreation
-              visible={isVisibleDialog}
-              close={() => toggleVisibleDialog(false)}
-            />
+            {(selectedOption.value === "tobaccos" && (
+              <TobaccosList tobaccos={productList} />
+            )) ||
+              (selectedOption.value === "coals" && (
+                <CoalList coals={productList} />
+              ))}
+            {rightsCheck(RoleCodes.moderator) && (
+              <>
+                <button
+                  className="for-hookah__button for-hookah__button--add"
+                  onClick={() => toggleVisibleDialog(true)}
+                >
+                  +
+                </button>
+                <EntitySelectionAndCreation
+                  visible={isVisibleDialog}
+                  close={() => toggleVisibleDialog(false)}
+                />
+              </>
+            )}
           </>
-        )}
+        </TabPanel>
       </div>
     </>
   );
