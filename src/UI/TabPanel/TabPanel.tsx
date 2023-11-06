9be84d11b146bafa3035ff9547900f676dc74099
Iamaletdinov Artur <willbefine1@gmail.com>
2023-11-06 18:17:47 +0500

    TabPanel - принимает пропсом отображаемые панели

--- src/UI/TabPanel/TabPanel.tsx
+++ src/UI/TabPanel/TabPanel.tsx
@@ -6,12 +6,14 @@ interface TabPanelProps {
   options: SelectOption[];
   onClick: (option: SelectOption) => void;
   defaultOption?: SelectOption;
+  children: JSX.Element;
 }
 
 export const TabPanel = ({
   options,
   onClick,
   defaultOption,
+  children,
 }: TabPanelProps) => {
   const [selectedTab, toSelectTab] = useState<SelectOption>(
     defaultOption ?? options[0]
@@ -26,11 +28,13 @@ export const TabPanel = ({
 
   return (
     <div className="tab-panel">
-      <ul className="tab-panel__tabs-list">
+      <ul className="tab-panel__titles-list">
         {options.map((opt: SelectOption) => (
           <li
-            className={`tab-panel__tab${
-              selectedTab.value === opt.value ? " tab-panel__tab--selected" : ""
+            className={`tab-panel__title${
+              selectedTab.value === opt.value
+                ? " tab-panel__title--selected"
+                : ""
             }`}
             key={opt.value}
             onClick={() => toClickOnTab(opt)}
@@ -39,6 +43,7 @@ export const TabPanel = ({
           </li>
         ))}
       </ul>
+      <div className="tab-panel__tabs-place">{children}</div>
     </div>
   );
 };
